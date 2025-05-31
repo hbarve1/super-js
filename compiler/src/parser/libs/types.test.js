49214de93204309c77a97f0a3a6c5a8894d59675
Himank Barve <hbarve1592@gmail.com>
2025-05-31 22:28:51 +0530

    parser types tests added

--- compiler/src/parser/libs/types.test.js
+++ compiler/src/parser/libs/types.test.js
@@ -56,4 +56,149 @@ describe('Parser - Type Annotations', () => {
             typeParams: [ { type: 'TypeIdentifier', name: 'T' } ]
         });
     });
-}); 
+    test('parses array type annotation', () => {
+        const code = 'let arr: number[] = [1, 2, 3];';
+        const lexer = new Lexer(code);
+        const tokens = lexer.tokenize();
+        const parser = new Parser(tokens);
+        const ast = parser.parse();
+        expect(ast.body[0].varType).toEqual({
+            type: 'ArrayType',
+            elementType: { type: 'TypeIdentifier', name: 'number' }
+        });
+    });
+    test('parses nested array and generic type annotation', () => {
+        const code = 'let arr: Array<Array<string>> = [["a"]];';
+        const lexer = new Lexer(code);
+        const tokens = lexer.tokenize();
+        const parser = new Parser(tokens);
+        const ast = parser.parse();
+        expect(ast.body[0].varType).toEqual({
+            type: 'GenericType',
+            name: 'Array',
+            typeParams: [
+                {
+                    type: 'GenericType',
+                    name: 'Array',
+                    typeParams: [ { type: 'TypeIdentifier', name: 'string' } ]
+                }
+            ]
+        });
+    });
+    test('parses object type annotation', () => {
+        const code = 'let obj: { p: number, q: string } = { p: 1, q: "a" };';
+        const lexer = new Lexer(code);
+        const tokens = lexer.tokenize();
+        const parser = new Parser(tokens);
+        const ast = parser.parse();
+        expect(ast.body[0].varType).toEqual({
+            type: 'ObjectType',
+            properties: [
+                { key: 'p', valueType: { type: 'TypeIdentifier', name: 'number' } },
+                { key: 'q', valueType: { type: 'TypeIdentifier', name: 'string' } }
+            ]
+        });
+    });
+    test('parses nested object type annotation', () => {
+        const code = 'let obj: { a: { b: number } } = { a: { b: 1 } };';
+        const lexer = new Lexer(code);
+        const tokens = lexer.tokenize();
+        const parser = new Parser(tokens);
+        const ast = parser.parse();
+        expect(ast.body[0].varType).toEqual({
+            type: 'ObjectType',
+            properties: [
+                {
+                    key: 'a',
+                    valueType: {
+                        type: 'ObjectType',
+                        properties: [
+                            { key: 'b', valueType: { type: 'TypeIdentifier', name: 'number' } }
+                        ]
+                    }
+                }
+            ]
+        });
+    });
+    test('parses parenthesized type annotation', () => {
+        const code = 'let x: (number) = 5;';
+        const lexer = new Lexer(code);
+        const tokens = lexer.tokenize();
+        const parser = new Parser(tokens);
+        const ast = parser.parse();
+        expect(ast.body[0].varType).toEqual({ type: 'TypeIdentifier', name: 'number' });
+    });
+    test('parses deeply nested type annotation', () => {
+        const code = 'let x: Array<{ a: number[] | string } & B> = [];' ;
+        const lexer = new Lexer(code);
+        const tokens = lexer.tokenize();
+        const parser = new Parser(tokens);
+        const ast = parser.parse();
+        expect(ast.body[0].varType).toEqual({
+            type: 'GenericType',
+            name: 'Array',
+            typeParams: [
+                {
+                    type: 'IntersectionType',
+                    left: {
+                        type: 'ObjectType',
+                        properties: [
+                            {
+                                key: 'a',
+                                valueType: {
+                                    type: 'UnionType',
+                                    left: {
+                                        type: 'ArrayType',
+                                        elementType: { type: 'TypeIdentifier', name: 'number' }
+                                    },
+                                    right: { type: 'TypeIdentifier', name: 'string' }
+                                }
+                            }
+                        ]
+                    },
+                    right: { type: 'TypeIdentifier', name: 'B' }
+                }
+            ]
+        });
+    });
+    test('parses type alias declaration', () => {
+        const code = 'type Foo = number;';
+        const lexer = new Lexer(code);
+        const tokens = lexer.tokenize();
+        const parser = new Parser(tokens);
+        const ast = parser.parse();
+        const typeDecl = ast.body.find(n => n.type === 'TypeDeclaration' && n.kind === 'type');
+        expect(typeDecl).toBeDefined();
+        expect(typeDecl.id).toBe('Foo');
+    });
+    test('parses interface declaration', () => {
+        const code = 'interface Bar { x: number; y: string; }';
+        const lexer = new Lexer(code);
+        const tokens = lexer.tokenize();
+        const parser = new Parser(tokens);
+        const ast = parser.parse();
+        const iface = ast.body.find(n => n.type === 'InterfaceDeclaration' && n.kind === 'interface');
+        expect(iface).toBeDefined();
+        expect(iface.id).toBe('Bar');
+    });
+    test('parses enum declaration', () => {
+        const code = 'enum Color { Red, Green, Blue }';
+        const lexer = new Lexer(code);
+        const tokens = lexer.tokenize();
+        const parser = new Parser(tokens);
+        const ast = parser.parse();
+        const en = ast.body.find(n => n.type === 'EnumDeclaration' && n.kind === 'enum');
+        expect(en).toBeDefined();
+        expect(en.id).toBe('Color');
+    });
+    test('parses namespace declaration', () => {
+        const code = 'namespace NS { export const z = 1; }';
+        const lexer = new Lexer(code);
+        const tokens = lexer.tokenize();
+        const parser = new Parser(tokens);
+        const ast = parser.parse();
+        const ns = ast.body.find(n => n.type === 'NamespaceDeclaration' && n.kind === 'namespace');
+        expect(ns).toBeDefined();
+        expect(ns.id).toBe('NS');
+    });
+});
